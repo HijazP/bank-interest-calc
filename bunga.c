3712b7ffbae9d677652fadb28f31e331f644888a
Hanif Ali Ramdani <hanifaliramdani98@gmail.com>
2024-02-16 20:55:25 +0700

    update printf biar ga lupa input apa

--- bunga.c
+++ bunga.c
@@ -4,6 +4,7 @@ int main()
 {
     double totalBunga = 0, bunga, pajakBunga, saldo;
 
+    printf("Mulai:\n");
     scanf("%lf", &saldo);
 
     while (saldo != 0)
@@ -11,6 +12,7 @@ int main()
         bunga = saldo * 0.00172;
         pajakBunga = bunga * 0.2;
         totalBunga = totalBunga + (bunga - pajakBunga);
+        printf("Masukkan angka 0 jika sudah selesai\n");
         scanf("%lf", &saldo);
     }
 
